14263c11cbbc96fcde9a6f610eca09b4e77c6d19
EzgiDinc <ezgiden@gmail.com>
2014-04-24 18:54:00 +0300

    Arayüz Dosyaları
    Arayüz dosyaları eklendi.

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -11,7 +11,29 @@ class DemoController < ApplicationController
 
   end
   
+   def egitim
 
+  end
+
+  def ilgi
+
+  end
+
+  def onlineCv
+
+  end
+
+  def referanslar
+
+  end
+
+  def sertifikalar
+
+  end
+
+  def tecrübeler
+
+  end  
 
 
 end
